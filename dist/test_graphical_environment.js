31d45ab018bd8a7fe98c1bde2731684c07b5a5d8
José Luis Fernández Aguilera <jose.fernandez1@suse.com>
2025-03-14 13:34:19 +0100

    Fixed close desktop button click

--- dist/test_graphical_environment.js
+++ dist/test_graphical_environment.js
@@ -628,7 +628,7 @@ exports.SoftwareSelectionPage = void 0;
 class SoftwareSelectionPage {
     page;
     patternText = (pattern) => this.page.locator(`::-p-aria(Select ${pattern})`);
-    closeButton = () => this.page.locator("button::-p-text(Close)");
+    closeButton = () => this.page.locator("::-p-text(Close)");
     constructor(page) {
         this.page = page;
     }
