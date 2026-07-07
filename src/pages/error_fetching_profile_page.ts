5cba22d0e4f5f01c39c8135f699a5cc18b00703f
Huajian.Luo <hluo@suse.com>
2026-07-07 12:42:42 +0800

    Adapt error fetching profile for 16 QU
    - Adapt error fetching profile for 16 QU
    - Related ticket: Quick PR

--- src/pages/error_fetching_profile_page.ts
+++ src/pages/error_fetching_profile_page.ts
@@ -1,24 +1,13 @@
 import { type Page } from "puppeteer-core";
-import assert from "node:assert/strict";
 
 export class ErrorFetchingProfilePage {
   private readonly page: Page;
-  private readonly alertWarningMsg = () =>
+  public readonly alertWarningMsg = () =>
     this.page.locator(
-      "::-p-text(Configuration cannot be applied because it is invalid or could not be reached)",
+      "::-p-text(Configuration cannot be applied because it is invalid or could not be reached), ::-p-text(It was unreachable or invalid)",
     );
 
   constructor(page: Page) {
     this.page = page;
-  }
-
-  async verifyContent() {
-    const elementText = await this.alertWarningMsg()
-      .map((span) => span.textContent)
-      .wait();
-    await assert.match(
-      elementText,
-      /Configuration cannot be applied because it is invalid or could not be reached/,
-    );
   }
 }
